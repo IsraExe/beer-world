7e2004cfda30b757a82bdd6c39b93d4bd40daaaa
IsraExe <israel-w@hotmail.com>
2024-07-21 18:57:07 -0300

    Chore: migrate database from SQLite to PostgreSQL

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-<p align="center">
-  <img width="260" src="https://raw.githubusercontent.com/myTapp/temos-vagas/master/logo_mytapp_primario.png?raw=true">
-</p>
-
-## O Teste full-stack developer (v1.2)
-O teste consiste em criar uma aplicação com Node.js e PostgreSQL que expõe uma API REST de um CRUD de usuário e uma aplicação web contendo uma interface para login e acesso a dados de uma API externa.
-
-Depois de logado o usuário da aplicação web deve poder acessar dados da [API](https://app2.mytapp.com.br/api/app/getTaps?e_id=456).
-
-### Back-end
-- Todos os endpoints de consulta de dados devem ter autenticação por webtoken ou similar
-- Fique a vontade para usar algum ORM de preferência ([sequelize](https://github.com/sequelize/sequelize), [typeorm](https://github.com/typeorm/typeorm), ...)
-
 > O CRUD de usuários não necessita interface, coloque os endpoints disponíveis no README do projeto.
+
+### Backend 
+
+1 - Abra o terminal e digite "npm i" para baixar todas as dependências do projeto
+2 - 
 
 ### Front-end
 O front-end deve apresentar pelo menos os seguintes requisitos:
--- frontend/src/components/CollapsibleTable/CollapsibleTable.tsx
+++ frontend/src/components/CollapsibleTable/CollapsibleTable.tsx
@@ -1,54 +1,11 @@
 'use client'
 
-import { Fragment, useMemo, useState } from 'react';
-import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Paper, CircularProgress } from '@mui/material';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useMemo, useState } from 'react';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, CircularProgress } from '@mui/material';
 import useFetch from '@/hooks/useFetch';
-import Pagination from '../Pagination/Pagination';
+import Pagination from './Pagination/Pagination';
 
-function Row(props: { row: any }) {
-  const { row } = props;
-  const [open, setOpen] = useState(false);
-
-  return (
-    <Fragment>
-      <TableRow>
-        <TableCell>
-          <IconButton aria-label='expand row' size='small' onClick={() => setOpen(!open)}>
-            {open ? <KeyboardArrowUpIcon sx={{ color: 'white' }} /> : <KeyboardArrowDownIcon sx={{ color: 'white' }} />}
-          </IconButton>
-        </TableCell>
-        <TableCell component='th' scope='row'>{row['beer_name']}</TableCell>
-        <TableCell align='right'>{row['beer_alcohol']}</TableCell>
-        <TableCell align='right'>{row['beer_style']}</TableCell>
-        <TableCell align='right'>{row['brewery']}</TableCell>
-        <TableCell align='right'>{row['beer_ibu']}</TableCell>
-        <TableCell align='right'>{row['price_ml']}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
-          <Collapse in={open} timeout='auto' unmountOnExit>
-            <Box sx={{ margin: 1, overflowX: 'auto' }}>
-              <Typography variant='h6' gutterBottom component='div'>
-                Descrição
-              </Typography>
-              <Table size='small' aria-label='purchases'>
-                <TableBody>
-                  <TableRow>
-                    <TableCell component='th' scope='row'>
-                      {row['beer_description'] || 'Sem descrição disponível'}
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </Fragment>
-  );
-}
+import Row from './Row/Row';
 
 export default function CollapsibleTable() {
 
