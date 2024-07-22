4a33a3a2914b898da4083451829ca8e8e9f6154a
IsraExe <israel-w@hotmail.com>
2024-07-22 01:46:04 -0300

    Cleanup: unused code

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -8,8 +8,6 @@ const errorHandler = async (error, req, res, next) => {
     if (error.code === DB_ERROR_CODE.conflict) return res.status(409).send({ error: `The ${error.meta.target} already exists!` });
 
     if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') return res.status(401).send({ error: 'Invalid token' });
-
-    console.log(error)
     
     const statusCode = error?.statusCode || 500;
     const messageError = error?.statusCode ? error?.message : 'Server internal error';
