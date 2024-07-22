4727c5919ea51168161e85c5059518990514df56
IsraExe <israel-w@hotmail.com>
2024-07-22 00:04:32 -0300

    Fixed: sameSite none cookies

--- backend/src/helpers/cookiesOptions.js
+++ backend/src/helpers/cookiesOptions.js
@@ -3,6 +3,8 @@ const cookiesOptions = (host) => {
     const domain = host === 'localhost' ? host : '.onrender.com';
     const secure = host === 'localhost' ? false : true;
 
+    console.log(host.slice(host.indexOf('.'), host.length));
+
     const oneDay = 1000 * 60 * 60 * 24;
     const cookieOptions = {
         httpOnly: true,
